345216d0f86f8079e9042dad7eb95cd6ee1957d9
indrapradesa <indrapradesa77@gmail.com>
2024-06-26 11:51:55 +0700

    bug search history paid

--- resources/views/partners/paid-histories/histories-paid.blade.php
+++ resources/views/partners/paid-histories/histories-paid.blade.php
@@ -15,7 +15,7 @@
     <div class="container mb-3">
         <div class="row justify-content-center">
             <div class="col-md-2 mt-2">
-                <form action="/partners/payments">
+                <form action="/partners/payments/histories-paid">
                     <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                     <div class="relative">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
